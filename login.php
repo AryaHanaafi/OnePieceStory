4214d40c1a31835a60b4977a7332920be7d409e4
AryaHanaafi <aryahanaafi115@gmail.com>
2024-12-27 16:31:03 +0700

    ubah judul form login

--- login.php
+++ login.php
@@ -85,7 +85,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <div class="card-body">
               <div class="text-center mb-3">
                 <i class="bi bi-person-circle h1 display-4"></i>
-                <p>Login</p>
+                <p>One Piece Story</p>
                 <hr />
               </div>
               <!-- Tempat untuk menampilkan pesan -->
